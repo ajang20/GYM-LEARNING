959903299e06c05ddf35855b0ddd0db39506a559
Ajang Samuel <ajangkorenilo8@gmail.com>
2025-11-22 21:35:14 +0200

    Added an asynchronous Exercise Folder

---
+++ Asynchronous Exercises/Code that fetches from many URLS and return the fastest returned.js
@@ -0,0 +1,9 @@
+// Create a function called `getFastPosts`that fetches posts 
+// from multiple endpoints simultaneously and returns data 
+// from the fastest source.
+
+// const apiUrls = [
+//   'https://jsonplaceholder.typicode.com/posts/4',
+//   'https://jsonplaceholder.typicode.com/posts/5',
+//   'https://jsonplaceholder.typicode.com/posts/6'
+// ];
